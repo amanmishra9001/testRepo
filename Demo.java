282842bfad11dea33c2d49f5bc05f29ae82bc503
Aman <amanstudent9001@gmail.com>
2024-01-16 12:59:02 +0530

    demo updated by akash

--- Demo.java
+++ Demo.java
@@ -9,7 +9,7 @@ public class Test {
 		//ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 //		Employee bean = (Employee) context.getBean("emp");
 		//Employee bean = context.getBean(Employee.class);
-		System.out.println("bank");
+		System.out.println("demo modified by akash bank");
 		
 	}
 
