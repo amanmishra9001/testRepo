b0774c2e6e7afe4751bf6febcb315b16b5ee3331
Aman <amanstudent9001@gmail.com>
2024-01-16 10:59:03 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,16 @@
+//package com.qsp.model;
+
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		//ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+//		Employee bean = (Employee) context.getBean("emp");
+		//Employee bean = context.getBean(Employee.class);
+		System.out.println("bank");
+		
+	}
+
+}
